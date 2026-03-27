984726e643b07624b858abffc7f07e406a4b849a
david-kajinake <davidkajinakeschenezare@gmail.com>
2026-03-27 23:29:51 +0300

    Removed task & greet apps

--- greet/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("isyeartwentytwentyfive/", views.isyeartwentytwentyfive , name = "index" ) ,
-    path("<str:name>/" , views.greet , name = "greet") ,
-]
